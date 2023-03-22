2ef03922614f488e3919567edb3c2683ea77ff81
Josea M <92557972+josea17m@users.noreply.github.com>
2023-03-22 17:50:58 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      {/* NavBar */}
+
+      <div>NavBar</div>
+
+      <main>
+        
+        {/* Input Nota */}
+        <div></div>
+
+        {/* Notas */}
+        <div></div>
+      </main>
+    </>
+  );
+}
+
+export default App;
